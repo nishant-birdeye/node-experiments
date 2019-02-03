e1099ae0d953c5ac6deb7b9accd52feee58a5c9d
nishant <nishant.singh@birdeye.com>
2019-02-03 16:45:29 +0530

    Add email to user schema.

--- server/modules/users/user.model.js
+++ server/modules/users/user.model.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: 'String', required: true },
+  email: { type: 'String', required: true },
   profileImage: { type: 'String', required: true },
   dateAdded: { type: 'Date', default: Date.now, required: true },
 });
